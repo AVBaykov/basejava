68778a47259201f5f2b53138ec62738f54aee582
Alexander <alexandr.baykov@gmail.com>
2018-05-22 22:15:08 +0300

    Убрали тестовый метод за ненадобностью

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,63 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        storage = new Resume[10000];
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length ; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
+        Resume absence = new Resume();
+        absence.uuid = "Нет резюме с таким id";
+        return absence;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) break;
+
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[i+1];
+
+                for (int j = i+1; j < size(); j++) {
+                    storage[j] = storage[j+1];
+                }
+
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+        if (size() > 0) {
+            return Arrays.copyOf(storage, size());
+        }
         return new Resume[0];
     }
 
     int size() {
-        return 0;
+        int count = 0;
+
+        for (Resume res : storage) {
+            if (res != null) count++;
+            else break;
+        }
+
+        return count;
     }
 }
