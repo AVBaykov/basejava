a18e962e26da1c49c817fbf12b6b0e63ed824d9b
ABaykov <alexandr.baykov@gmail.com>
2018-07-08 15:47:54 +0300

    - Lesson 6 HW. Refactored Resume class

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -6,23 +6,21 @@ import java.util.UUID;
 
 public class Resume {
     private final String uuid;
-    private String fullName;
+    private final String fullName;
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
     }
 
     public Resume(String uuid, String fullName) {
+        Objects.requireNonNull(uuid, "uuid must not be null");
+        Objects.requireNonNull(fullName, "fullName must not be null");
         this.uuid = uuid;
         this.fullName = fullName;
     }
 
     public String getFullName() {
         return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
     }
 
     public String getUuid() {
