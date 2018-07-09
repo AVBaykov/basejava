07060893b67e6f63a82ac4b0548395ccf19f1cad
ABaykov <alexandr.baykov@gmail.com>
2018-07-09 15:55:05 +0300

    - Lesson 7 added Logging

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -6,10 +6,13 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class AbstractStorage<SK> implements Storage {
+
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     protected abstract SK getKey(String uuid);
 
@@ -27,30 +30,35 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public final void update(Resume resume) {
+        LOG.info("Update " + resume);
         SK key = getIfExist(resume.getUuid());
         doUpdate(key, resume);
     }
 
     @Override
     public final void save(Resume resume) {
+        LOG.info("Save " + resume);
         SK key = getIfNotExist(resume.getUuid());
         doSave(key, resume);
     }
 
     @Override
     public final Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK key = getIfExist(uuid);
         return doGet(key);
     }
 
     @Override
     public final void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK key = getIfExist(uuid);
         doDelete(key);
     }
 
     private SK getIfExist(String uuid) {
         if (!isExists(getKey(uuid))) {
+            LOG.warning("Resume " + uuid + " not exist");
             throw new NotExistStorageException(uuid);
         }
         return getKey(uuid);
@@ -58,6 +66,7 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     private SK getIfNotExist(String uuid) {
         if (isExists(getKey(uuid))) {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new ExistStorageException(uuid);
         }
         return getKey(uuid);
@@ -65,6 +74,7 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         return getStreamForSort()
                 .sorted(Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid))
                 .collect(Collectors.toList());
