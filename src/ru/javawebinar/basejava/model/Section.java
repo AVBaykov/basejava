c771db2ad6dfd8c91d2b10b4a88695d644f8a4d7
ABaykov <alexandr.baykov@gmail.com>
2018-07-10 23:36:49 +0300

    - Lesson 07 HW. Added Section abstract class

---
+++ src/ru/javawebinar/basejava/model/Section.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.basejava.model;
+
+public abstract class Section {
+    private final SectionType type;
+    private final String title;
+
+    Section(SectionType type){
+        this.type = type;
+        this.title = type.getTitle();
+    }
+
+    public SectionType getType() {
+        return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
