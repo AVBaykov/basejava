c1baccfddd110945b542a02f5c1a0e3490bf9dec
ABaykov <alexandr.baykov@gmail.com>
2018-07-10 23:38:44 +0300

    - Lesson 07 HW. Added ParagraphSection class

---
+++ src/ru/javawebinar/basejava/model/ParagraphSection.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.basejava.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParagraphSection extends Section {
+    private List<Paragraph> paragraphList = new ArrayList<>();
+
+    ParagraphSection(SectionType type) {
+        super(type);
+    }
+
+    private class Paragraph {
+        private String paragraphName;
+        private List<String> description = new ArrayList<>();
+    }
+}
