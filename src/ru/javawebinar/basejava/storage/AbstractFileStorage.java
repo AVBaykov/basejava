1b76f9e6d65176e2a90e33e0cb6a22d428d5d683
ABaykov <alexandr.baykov@gmail.com>
2018-07-18 23:45:50 +0300

    -Lesson 08 HW.Added AbstractFileStorage class/implemented methods

---
+++ src/ru/javawebinar/basejava/storage/AbstractFileStorage.java
@@ -0,0 +1,93 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.exception.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    protected abstract void doWrite(File file, Resume resume) throws IOException;
+
+    protected abstract Resume doRead(File file);
+
+    @Override
+    protected File getKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected void doUpdate(File file, Resume resume) {
+        try {
+            doWrite(file, resume);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    @Override
+    protected boolean isExists(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected void doSave(File file, Resume resume) {
+        try {
+            file.createNewFile();
+            doWrite(file, resume);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+
+    }
+
+    @Override
+    protected Resume doGet(File file) {
+        return doRead(file);
+    }
+
+    @Override
+    protected void doDelete(File file) {
+        file.delete();
+    }
+
+    @Override
+    Stream<Resume> getStreamForSort() {
+        List<Resume> result = new ArrayList<>();
+        for (File file : directory.listFiles()) {
+            result.add(doRead(file));
+        }
+        return result.stream();
+    }
+
+    @Override
+    public void clear() {
+        for (File file : directory.listFiles()) {
+            if (file.isFile()) {
+                file.delete();
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        return directory.listFiles().length;
+    }
+}
